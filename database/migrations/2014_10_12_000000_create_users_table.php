a3910f393eff7f3441fd5cf53316261d3f09514f
Ronildo Sousa <sousaronildo400@gmail.com>
2022-10-06 14:56:45 -0300

    socialite + user migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable(bcrypt("password"));
+            $table->string("auth_provider");
+            $table->string("auth_id");
+            $table->string("auth_secret");
             $table->timestamps();
         });
     }
