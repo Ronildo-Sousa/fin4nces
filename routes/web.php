6d940c84b6b586555cb6a17c49837f4476f0d8b3
Ronildo Sousa <sousaronildo400@gmail.com>
2022-10-11 19:01:30 -0300

    guest middleware

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,28 @@ use App\Http\Livewire\Pages\Dashboard;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 
-Route::get('/', Login::class)->name('login');
-Route::get('/register', Register::class)->name('register');
+#region Guest routes
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', Login::class)->name('login');
+    Route::get('/register', Register::class)->name('register');
 
-Route::get('/auth/{provider}', function (string $provider) {
-    return Socialite::driver($provider)->redirect();
-})->name('SocialRedirect');
+    Route::get('/auth/{provider}', function (string $provider) {
+        return Socialite::driver($provider)->redirect();
+    })->name('SocialRedirect');
 
-Route::get('/{provider}/callback', function (string $provider) {
-    if((new AuthSocial)->run($provider)){
-        return redirect()->route('dashboard');
-    }
-    session()->flash('error', 'Could not authenticate this user');
-    return redirect()->route('login');
-})->name('Authsocial');
+    Route::get('/{provider}/callback', function (string $provider) {
+        if ((new AuthSocial)->run($provider)) {
+            return redirect()->route('dashboard');
+        }
+        session()->flash('error', 'Could not authenticate this user');
+        return redirect()->route('login');
+    })->name('Authsocial');
+});
+#endregion
 
+
+#region Auth routes
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
 });
+#endregion
